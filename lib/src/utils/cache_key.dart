126e437872b5a078263fb9da4ac5f09faad2739a
gonuit <kamilklyta341@gmail.com>
2020-09-07 11:17:06 +0200

    Make cache keys operates on strings.

--- lib/src/utils/cache_key.dart
+++ lib/src/utils/cache_key.dart
@@ -3,7 +3,7 @@ part of http_api;
 const _m = 0x5bd1e995;
 const _r = 24;
 int _simpleHash(int value, [int seed = 0]) {
-  var len = 4; // int length 32 bit
+  var len = 4; // int length 32 bit (assumption).
   var h = seed ^ len;
 
   var k = value;
@@ -15,8 +15,6 @@ int _simpleHash(int value, [int seed = 0]) {
   h *= _m;
   h ^= k;
 
-  len -= 4;
-
   h ^= h >> 13;
   h *= _m;
   h ^= h >> 15;
@@ -25,15 +23,18 @@ int _simpleHash(int value, [int seed = 0]) {
 }
 
 @immutable
-class CacheKey<T> {
-  final T value;
+class CacheKey {
+  final String value;
   const CacheKey(this.value);
 
   @override
   bool operator ==(Object other) =>
       other.runtimeType == runtimeType &&
-      other is CacheKey<T> &&
+      other is CacheKey &&
       other.value == value;
 
   int get hashCode => _simpleHash(value.hashCode, runtimeType.hashCode);
+
+  @override
+  String toString() => "$runtimeType('$value')";
 }
